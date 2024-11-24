2abcb001461e012982b58af8e66ff12acef84727
tusharchhabra-coder <tchhabra0739@gmail.com>
2024-11-25 01:44:00 +0530

    Add files via upload

---
+++ exp1q4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    printf("Enter a number: ");
+    scanf("%d", &N);
+
+    if (N % 2 == 0)
+        printf("%d is Even.\n", N);
+    else
+        printf("%d is Odd.\n", N);
+
+    return 0;
+}
